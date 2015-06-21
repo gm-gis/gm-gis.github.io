afc7e8061080855dfbb3163552ba5bffbfacdef8
gmgis <gmilb41@gmail.com>
2015-06-21 18:47:30 -0400

    geojson stuff

---
+++ places/places.js
@@ -0,0 +1,309 @@
+var points= {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "ESRI",
+        "role": "Technical Marketing Intern",
+        "time": "Summer 2014",
+        "type": "work",
+        "img": "<img src='https://officepictures.files.wordpress.com/2012/02/front-view.jpg'/>"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -117.19568967819214,
+          34.05641053454641
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Penn State University",
+        "role": "Research Assistant",
+        "time": "December 2013-Present",
+        "type": "work",
+        "img": "<img src='http://onwardstate.com/wp-content/uploads/2012/11/580x385x580171_10151916746700663_1092465643_n.jpeg.pagespeed.ic.PVobPyxaRG.jpg'/>"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -77.86687731742859,
+          40.79325436303818
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "State College Borough",
+        "role": "GIS Intern",
+        "time": "Fall 2013",
+        "type": "work",
+        "img": "<img src='https://www.downtownstatecollege.com/images_home/1.jpg'/>"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -77.85886287689209,
+          40.792815746526834
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Summer's Best Two Weeks",
+        "role": "Camp Counselor",
+        "time": "Summer 2012, 2013",
+        "type": "work",
+        "img": "<img src='https://scontent-a-lga.xx.fbcdn.net/hphotos-xfa1/v/t1.0-9/1609964_848105808573761_2541637874081693742_n.jpg?oh=754e1bf39a64a5e4fd365f0ad7e28457&oe=55530364'/>"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -79.09675598144531,
+          40.155392485732726
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Grand Canyon",
+        "role": "5-Day Backpacking Tip",
+        "time": "January 2015",
+        "type": "Travel",
+        "img": "<img src='https://fbcdn-sphotos-e-a.akamaihd.net/hphotos-ak-xpa1/v/t1.0-9/10888687_4992359182326_348458772471323877_n.jpg?oh=169385435053619f91230670fceebd59&oe=556AA9CB&__gda__=1431700406_756ab8297e4507e80a0b97c56c74db54'/>"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -112.09625244140625,
+          36.04743345598549
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Canyonlands National Park",
+        "role": "Hiking, Exploring",
+        "time": "May 2014, January 2015",
+        "type": "Travel",
+        "img": "<img src='https://fbcdn-sphotos-g-a.akamaihd.net/hphotos-ak-xpa1/v/t1.0-9/10371666_4210516516748_6012905727326739270_n.jpg?oh=4234af2a895b14d3ad7ce521f9b23c45&oe=555DD52F&__gda__=1431275693_764426896ea455ce53e5a2701d107ba9'/>"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -109.86396789550781,
+          38.30825817353996
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Arches National Park",
+        "role": "Exploring",
+        "time": "May 2014, January 2015",
+        "type": "Travel",
+        "img": "<img src='https://fbcdn-sphotos-b-a.akamaihd.net/hphotos-ak-xap1/v/t1.0-9/10404419_4992375782741_4142529354447285014_n.jpg?oh=6126f9319555b9655a190229d988fd4d&oe=55645899&__gda__=1431458660_493a7abeefe525ec5f25fda603b3cbfa'/>"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -109.5347213745117,
+          38.68510776818861
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Pacific Coast Highway",
+        "role": "2-Day Road Trip",
+        "time": "July 2014",
+        "type": "Travel",
+        "img": "<img src='https://scontent-b-lga.xx.fbcdn.net/hphotos-xfp1/v/t1.0-9/10464136_4368533907084_4178925375455562143_n.jpg?oh=6b97426051608c160512382f63000f49&oe=55680A6D'/>"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -121.89125061035155,
+          36.33946565299958
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Yosemite",
+        "role": "Hiking, Exploring",
+        "time": "August 2014",
+        "type": "Travel",
+        "img": "<img src='https://fbcdn-sphotos-c-a.akamaihd.net/hphotos-ak-xpa1/v/t1.0-9/10616428_4504384303259_3230157800133639207_n.jpg?oh=36e21b316134b0230638469aa5e72855&oe=555B6975&__gda__=1431541751_335cbf82734cc8339b6a43a3ca5f21f1'/>"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -119.57107543945311,
+          37.72782336496339
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Wasatch Mountains",
+        "role": "Cross-Country Road Trip",
+        "time": "August 2014",
+        "type": "Travel",
+        "img": "<img src='https://fbcdn-sphotos-f-a.akamaihd.net/hphotos-ak-xfp1/v/t1.0-9/10612746_4504438984626_3230283233246349264_n.jpg?oh=43b5972d58667119d35f1090de88e21b&oe=5553EC13&__gda__=1431938066_d1e1dfc1cca3e42ae16ee6dbdeb256e8'/>"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -111.38900756835938,
+          40.46889024168825
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Rocky Mountain National Park",
+        "role": "Hiking, Camping",
+        "time": "August 2014",
+        "type": "Travel",
+        "img": "<img src='https://scontent-a-lga.xx.fbcdn.net/hphotos-xaf1/v/t1.0-9/10559863_4504467945350_7608540977612725056_n.jpg?oh=ad514a1d8f63a071c72d4c57ecd66c2b&oe=556646AB'/>"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -105.65483093261719,
+          40.30963984228559
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Oklahoma City",
+        "role": "OKC Thunder Basketball Game (Road Trip)",
+        "time": "January 2015",
+        "type": "Travel",
+        "img": "<img src='https://fbcdn-sphotos-f-a.akamaihd.net/hphotos-ak-xpa1/v/t1.0-9/10924775_4992355942245_6783671512845555628_n.jpg?oh=876fade88d2095f140e1db96e5e26118&oe=5561E8CB&__gda__=1431315017_70d219b885d5ff9cd4f1ff4346c34c95'/>"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -97.51512050628662,
+          35.46383333003051
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Joshua Tree National Park",
+        "role": "Hiking, Avoiding Rattlesnakes",
+        "time": "May 2014",
+        "type": "Travel",
+        "img": "<img src='https://fbcdn-sphotos-b-a.akamaihd.net/hphotos-ak-xpf1/v/t1.0-9/10245404_4210528637051_4261430365212744654_n.jpg?oh=c897b151bbe0beaff6c5050c271c77c6&oe=556B16A4&__gda__=1433272392_6607be27e2cd6c6807ef52048ddc609b'/>"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -116.18574142456055,
+          34.04014265821752
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Birmingham, Alabama",
+        "role": "Serving adults with disabilities",
+        "time": "May 2014",
+        "type": "Service",
+        "img": "<img src='http://www.physician-innovations.com/UCP_Logo.gif'/>"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -86.87465786933899,
+          33.433205197251446
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Stowe, Vermont",
+        "role": "XC Skiing, Snowshoeing",
+        "time": "December 2014",
+        "type": "Travel",
+        "img": "<img src='https://scontent-a-lga.xx.fbcdn.net/hphotos-xaf1/v/t1.0-9/10881500_10205898373366810_2769806037817003753_n.jpg?oh=a6016bc6c66c2647cee33eb220cb54a6&oe=55608A51'/>"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -72.74236679077148,
+          44.46916310149303
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Acadia National Park",
+        "role": "Hiking, Kayaking",
+        "time": "July 2010",
+        "type": "Travel",
+        "img": "<img src='https://scontent-a-lga.xx.fbcdn.net/hphotos-xaf1/v/t1.0-9/39650_1535966527438_3193345_n.jpg?oh=133cb038d3b57fcf5d9749237c3dc273&oe=555B805D'/>"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -68.21617126464844,
+          44.356751085978146
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "La Republica Dominicana",
+        "role": "Service Trip",
+        "time": "March 2011",
+        "type": "Service"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -70.64809799194336,
+          19.42661072109788
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Kensington, Philadelphia",
+        "role": "Inner-City Missions",
+        "time": "Various, 2009-2011",
+        "type": "Service"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -75.10794639587401,
+          39.99405432781469
+        ]
+      }
+    }
+  ]
+};
---
+++ places/travels.geojson
@@ -0,0 +1,311 @@
+{
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "ESRI",
+        "role": "Technical Marketing Intern",
+        "time": "Summer 2014",
+        "type": "work",
+        "img": "<img src='https://officepictures.files.wordpress.com/2012/02/front-view.jpg'/>"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -117.19568967819214,
+          34.05641053454641
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Penn State University",
+        "role": "Research Assistant",
+        "time": "December 2013-Present",
+        "type": "work"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -77.86687731742859,
+          40.79325436303818
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "State College Borough",
+        "role": "GIS Intern",
+        "time": "Fall 2013",
+        "type": "work"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -77.85886287689209,
+          40.792815746526834
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Summer's Best Two Weeks",
+        "role": "Camp Counselor",
+        "time": "Summer 2012, 2013",
+        "type": "work"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -79.09675598144531,
+          40.155392485732726
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Grand Canyon",
+        "role": "5-Day Backpacking Tip",
+        "time": "January 2015",
+        "type": "play",
+        "img": "<img src='https://fbcdn-sphotos-e-a.akamaihd.net/hphotos-ak-xpa1/v/t1.0-9/10888687_4992359182326_348458772471323877_n.jpg?oh=169385435053619f91230670fceebd59&oe=556AA9CB&__gda__=1431700406_756ab8297e4507e80a0b97c56c74db54'/>"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -112.09625244140625,
+          36.04743345598549
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Canyonlands National Park",
+        "role": "Hiking, Exploring",
+        "time": "May 2014, January 2015",
+        "type": "play"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -109.86396789550781,
+          38.30825817353996
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Arches National Park",
+        "role": "Exploring",
+        "time": "May 2014, January 2015",
+        "type": "play"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -109.5347213745117,
+          38.68510776818861
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Pacific Coast Highway",
+        "role": "2-Day Road Trip",
+        "time": "July 2015",
+        "type": "play"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -121.89125061035155,
+          36.33946565299958
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Yosemite",
+        "role": "Hiking, Exploring",
+        "time": "August 2014",
+        "type": "play"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -119.57107543945311,
+          37.72782336496339
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Pacific Coast Highway",
+        "role": "2-Day Road Trip",
+        "time": "July 2014",
+        "type": "play"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -119.26448822021483,
+          37.91603433975963
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Wasatch Mountains",
+        "role": "Cross-Country Road Trip",
+        "time": "August 2014",
+        "type": "play"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -111.38900756835938,
+          40.46889024168825
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Rocky Mountain National Park",
+        "role": "Hiking, Camping",
+        "time": "August 2014",
+        "type": "play"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -105.65483093261719,
+          40.30963984228559
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Oklahoma City",
+        "role": "OKC Thunder Basketball Game",
+        "time": "January 2015",
+        "type": "play"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -97.51512050628662,
+          35.46383333003051
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Joshua Tree National Park",
+        "role": "Hiking, Avoiding Rattlesnakes",
+        "time": "May 2014",
+        "type": "play"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -116.18574142456055,
+          34.04014265821752
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Birmingham, Alabama",
+        "role": "Serving adults with disabilities at United Cerebral Palsy",
+        "time": "May 2014",
+        "type": "service"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -86.87465786933899,
+          33.433205197251446
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Stowe, Vermont",
+        "role": "XC Skiing, Snowshoeing",
+        "time": "December 2014",
+        "type": "play"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -72.74236679077148,
+          44.46916310149303
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Acadia National Park",
+        "role": "Hiking, Kayaking",
+        "time": "July 2010",
+        "type": "play"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -68.21617126464844,
+          44.356751085978146
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "La Republica Dominicana",
+        "role": "Service Trip",
+        "time": "March 2011",
+        "type": "service"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -70.64809799194336,
+          19.42661072109788
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Kensington, Philadelphia",
+        "role": "Inner-City Missions",
+        "time": "Various, 2009-2011",
+        "type": "service"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -75.10794639587401,
+          39.99405432781469
+        ]
+      }
+    }
+  ]
+}
