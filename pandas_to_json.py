2c95553513426613ef0803329803c8fca592eac7
gm-gis <gmilb41@gmail.com>
2019-08-15 21:29:19 -0400

    adding json file to repo and test js can access it

---
+++ pandas_to_json.py
@@ -0,0 +1,22 @@
+#csv_to_json
+
+import csv
+import json
+import os
+import pandas as pd 
+
+in_file=r'C:\\Users\\Gregory\\Documents\\alumni_connect\\Contact.csv'
+
+data = []
+
+os.chdir('C:\\Users\\Gregory\\Documents\\alumni_connect')
+
+csvfile = open('Contact.csv', 'r', encoding='utf-8')
+jsonfile = open('Contacts_pandas.json', 'w', encoding='utf-8')
+
+df = pd.read_csv('Contact.csv')
+
+
+out_json = df.to_json(orient='records')
+
+jsonfile.write(out_json)
